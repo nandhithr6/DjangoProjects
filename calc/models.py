476798be8e596df70fb321f0088276106e18b8bc
nandhithr6 <ys.nandhitha@gmail.com>
2024-08-01 08:35:24 +0530

    customer table

--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name=models.CharField(max_length=300, null=True)
+    age=models.CharField(max_length=300, null=True)
+    date=models.DateTimeField(max_length=300, null=True)
+    
+    def __str__(self): return self.name
--- calc/urls.py
+++ calc/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('add', views.add, name='add'),
+    path('dashboard/',views.dashboard, name='dashboard'),
+    path('products/', views.products, name='products'),
 ]
